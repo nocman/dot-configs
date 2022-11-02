6ef261ebd8fe6b1983f5e8fc713ad0d0ab05b142
Robert Rędziak <robert.redziak@gmail.com>
2022-11-02 12:01:40 +0100

    Do not create symlink when exists. Add notice for Ubuntu.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+source /etc/os-release
+if [[ $ID == "ubuntu" ]]; then
+  echo "Add this snippet to your ~/.profile (not ~/.bashrc!):
+
+--8<--
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+  for rc in ~/.bashrc.d/*; do
+    if [ -f "$rc" ]; then
+      . "$rc"
+    fi
+  done
+fi
+
+unset rc
+--8<--
+
+"
+fi
+
 /usr/bin/which pip 1>/dev/null 2>/dev/null 
 if [[ $? > 0 ]]; then
   echo 'Install pip first.'
@@ -20,7 +40,10 @@ for i in ${!configs[@]}; do
   if [ ${pref} != '.' -a ! -d ~/${pref} ]; then
     /usr/bin/mkdir -p ~/${pref} 
   fi
-  /usr/bin/ln -s ${repodir}/${i} ~/${configs[${i}]}
+  if [ ! -L ~/${configs[${i}]} ]; then
+    echo "Linking ~/${configs[${i}]} to ${repodir}/${i}."
+    /usr/bin/ln -s ${repodir}/${i} ~/${configs[${i}]}
+  fi
 done
 
 if [ -f requirements.txt ]; then
